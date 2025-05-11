db7f6462adaaa21c80785b857f1ca2cc9f62cfb0
Mr-Lahey-jpg <holderm1309@gmail.com>
2025-05-11 18:02:10 -0400

    finished updating scoreboard!

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,23 @@
 from turtle import Turtle,Screen
 
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.write(arg="Score: ",align="center")
+        self.current_score = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(x=0,y=260)
+        self.pencolor("white")
+        self.write(arg=f"Score: 0",align="center",font=('arial',20,'normal'))
 
-scoreboard = Scoreboard()
+    def add_points(self):
+        self.current_score += 1
+        self.clear()
+        self.write(arg=f"Score: {self.current_score}", align="center", font=('arial', 20, 'normal'))
 
-screen = Screen()
-screen.exitonclick()
+
+
+
