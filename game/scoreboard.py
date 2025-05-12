04394f94352a01dcf3f97d5b1458472b0e38042a
Mr-Lahey-jpg <holderm1309@gmail.com>
2025-05-12 10:34:16 -0400

    project done
    project done

--- game/scoreboard.py
+++ game/scoreboard.py
@@ -1,22 +1,31 @@
 from turtle import Turtle,Screen
-
-
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.current_score = 0
+        self.score = 0
         self.hideturtle()
         self.penup()
         self.goto(x=0,y=260)
         self.pencolor("white")
-        self.write(arg=f"Score: 0",align="center",font=('arial',20,'normal'))
+        self.update_scoreboard()
 
-    def add_points(self):
-        self.current_score += 1
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align= ALIGNMENT, font= FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(arg=f"GAME OVER", align= ALIGNMENT, font= FONT)
+
+    def increase_score(self):
+        self.score += 1
         self.clear()
-        self.write(arg=f"Score: {self.current_score}", align="center", font=('arial', 20, 'normal'))
+        self.update_scoreboard()
+
+
 
 
 
--- main.py
+++ main.py
@@ -26,8 +26,22 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        scoreboard.add_points()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
